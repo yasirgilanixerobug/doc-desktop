b65801dca9cd41de5ded069e1e78009c267f73fe
Mohsin Shah <mohsin.edu1@gmail.com>
2025-09-25 18:46:00 +0500

    resolved issue of jquery not working

--- resources/views/layout/clinic.blade.php
+++ resources/views/layout/clinic.blade.php
@@ -59,6 +59,9 @@
 
 <!-- jQuery -->
 <script src="{{ asset('/clinic-assets/plugins/jquery/jquery.min.js')  }}"></script>
+<script>
+    window.$ = window.jQuery = require ? require('jquery') : window.jQuery;
+</script>
 <!-- Bootstrap 4 -->
 <script src="{{ asset('/clinic-assets/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 <!-- AdminLTE App -->
@@ -67,8 +70,9 @@
 <script src="{{ asset('/clinic-assets/custom/js/filter-search.js')  }}"></script>
 <!-- Toastr -->
 <script src="{{ asset('/clinic-assets/plugins/toastr/toastr.min.js') }}"></script>
-@include('flash-message')
 
+@include('flash-message')
+        
 @stack('js')
 </body>
 </html>
